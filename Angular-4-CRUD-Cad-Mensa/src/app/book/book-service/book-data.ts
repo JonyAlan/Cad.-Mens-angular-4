4f6e4d3cabd115b2d3845a91f629756f62cb10a5
JonyAlan <34633476+JonyAlan@users.noreply.github.com>
2018-06-21 14:04:52 -0300

    Trabalho de ES4 em angular 4
    comandos= npm install, ng serve

---
+++ Angular-4-CRUD-Cad-Mensa/src/app/book/book-service/book-data.ts
@@ -0,0 +1,81 @@
+/**
+ * 
+ * sample data
+ */
+import { Book } from './book';
+
+export const BOOK_LIST: Book[] = [
+          {
+          "id":1,
+          "cpf": 38885504881,
+          "nome": "Jony Alan de C.nunes",
+          "telefone": "(15) 798122211",
+          "marca": "Audi",
+          "placa": "AAA-1222",
+          "diavencimento": 19,
+          "situacao": "Areasado",
+          
+        },
+        {
+            "id":2,
+          "cpf": 38815504882,
+          "nome": "Rafel de Carvalho dos Santos",
+          "telefone": "(15) 998122211",
+          "marca": "Chevrolet",
+          "placa": "BBB-1254",
+          "diavencimento": 20,
+          "situacao": "Pago",       
+        },
+        {
+            "id":3,
+          "cpf": 388155048843,
+          "nome": "Felipe de Carvalho Nunes",
+          "telefone": "(15) 12122211",
+          "marca": "VW",
+          "placa": "CCC-2460",  
+          "diavencimento": 29,
+          "situacao": "",     
+        },
+        {
+            "id":4,
+          "cpf": 38815504886,
+          "nome": "Debora cristina",
+          "telefone": "(15) 993122211",
+          "marca": "VW",
+          "placa": "DDD-3254",    
+          "diavencimento": 29,
+          "situacao": "",      
+        },
+        {
+            "id":5,
+          "cpf": 81593277574,
+          "nome": "Renata de Almeida",
+          "telefone": "(15) 32311154",
+          "marca": "Renaut",
+          "placa": "EEE-1352",  
+          "diavencimento": 29,
+          "situacao": "",        
+        },
+        {
+            "id":6,
+          "cpf": 47814904244,
+          "nome": "Maria Meneses",
+          "telefone": "(15) 38182211",
+          "marca": "VW",
+          "placa": "FFF-2278",
+          "diavencimento": 29,
+          "situacao": "",
+        },
+        {
+            "id":7,
+          "cpf": 11449325862,
+          "nome": "Priscila fatni",
+          "telefone": "(15) 99122211",
+          "marca": "BMW",
+          "placa": "NNN-234",
+          "diavencimento": 29,
+          "situacao": "",
+        },
+       
+]
+      
---
+++ Angular-4-CRUD-Cad-Mensa/src/app/book/book-service/book.ts
@@ -0,0 +1,13 @@
+/**
+ * Book Interface
+ */
+export interface Book {
+    id:number,    
+    cpf: number,
+    nome: string,
+    telefone:string,
+    marca: string,   
+    placa: string,
+    diavencimento: number,
+    situacao: string,
+}
